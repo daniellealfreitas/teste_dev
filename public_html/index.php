c6c04abffe8d48702467c1235ac8093c962627d5
Daniel Leal Freitas <dbleal@hotmail.com>
2020-11-06 18:16:36 -0300

    housekepping

--- public_html/index.php
+++ public_html/index.php
@@ -22,24 +22,33 @@ $database = new \Nerdweb\Database($configDB);
     </head>
        
     <body>
+
+    <!-- Navigation -->
     <nav class="nav-extended">
-    <div class="nav-wrapper">
-    <a href="#!" class="brand-logo"><img src="assets\img\logo_nerdweb.png" alt=""></a>
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    <div class="nav-wrapper  blue lighten-4">
+    <a href="#!" class="brand-logo " style="padding:10px;"><img src="assets\img\logo_nerdweb.png" alt=""></a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons ">menu</i></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="#">Principal</a></li>
-        <li><a href="#">Noticias</a></li>
+        <li><a class="blue-text text-darken-3" href="#">Principal</a></li>
+        <li><a class="blue-text text-darken-3" href="#">Noticias</a></li>
       </ul>
     </div>
   </nav>
-
   <ul class="sidenav" id="mobile-demo">
     <li><a href="#">Principal</a></li>
     <li><a href="#">Noticias</a></li>
   </ul>
 
+  <!-- Display table of news -->
+
   
-  <footer class="page-footer">
+
+  <!-- Display form to insert news --> 
+
+
+
+  <!-- Footer -->
+  <footer class="page-footer  blue darken-2">
           <div class="container">
             <div class="row">
               <div class="col l6 s12">
@@ -57,7 +66,7 @@ $database = new \Nerdweb\Database($configDB);
               </div>
             </div>
           </div>
-          <div class="footer-copyright">
+          <div class="footer-copyright blue darken-3">
             <div class="container">
             © 2014 Copyright Text
             <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
